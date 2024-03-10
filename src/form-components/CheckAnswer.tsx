3c9f4b9a1ab8eea81c19c74c9efd1a4930bf5aa5
ejciarlo <89992730+ejciarlo@users.noreply.github.com>
2024-03-10 16:32:45 -0400

    Added functions

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Give your answer:</Form.Label>
+                <Form.Control
+                    type="string"
+                    value={answer}
+                    placeholder="Answer"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                ></Form.Control>
+            </Form.Group>
+            <div> {answer === expectedAnswer ? "✔️" : "❌"} </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsRemaining, setAttemptsRemaining] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<string>("0");
+
+    const handleGain = () => {
+        const parsedAttempts = parseInt(attemptsRequested);
+        if (!isNaN(parsedAttempts)) {
+            setAttemptsRemaining(attemptsRemaining + parsedAttempts);
+        }
+    };
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>How many attempts do you wish to gain?</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequested}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttemptsRequested(event.target.value)
+                    }
+                ></Form.Control>
+            </Form.Group>
+            <Button onClick={handleGain}>Gain</Button>
+            <Button
+                onClick={() => setAttemptsRemaining(attemptsRemaining - 1)}
+                disabled={attemptsRemaining === 0}
+            >
+                Use
+            </Button>
+            <span>Attempts Remaining: {attemptsRemaining}</span>
         </div>
     );
 }
